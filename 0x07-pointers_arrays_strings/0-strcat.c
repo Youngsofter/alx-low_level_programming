7cd4e829282b6deaa10185ea6edc02e1218cfa9b
youngsofter <ys.benfatih@gmail.com>
2023-04-03 16:54:34 -0700

    strcat

--- 0x07-pointers_arrays_strings/0-strcat.c
+++ 0x07-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,17 @@
-#include "main.h"
-
 /**
- * _memset - Fills memory with a constant byte.
- * @s: Pointer to the memory area.
- * @b: Constant byte to fill the memory area with.
- * @n: Number of bytes to fill.
- *
- * Return: Pointer to the memory area s.
- */
+* _memset - fills memory with a constant byte
+* @s: memory area to fill in
+* @b: constant byte to fill s with
+* @n: number of bytes to write
+*
+* Return: a pointer to the memory area s
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-        s[i] = b;
-    }
+	for (i = 0; i < n; i++)
+		s[i] = b;
 
-    return s;
+	return (s);
 }
-
