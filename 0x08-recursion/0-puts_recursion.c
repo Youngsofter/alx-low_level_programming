a2391067c475036ea44ad90d175538f6c30ad44c
youngsofter <ys.benfatih@gmail.com>
2023-04-05 17:40:58 -0700

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,20 @@
+#include "main.h"
 /**
- * _puts_recursion - prints a string
- * @s: An input string to print
+ * _puts_recursion - prints a string.
+ * @s: source string
  *
- * Return: void
+ *
+ * Return: the string printed.
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	else
-		_putchar('\n');
 }
